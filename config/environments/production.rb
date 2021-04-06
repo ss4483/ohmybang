1ca912ca8e4258a48e1526749285c22628d590c8
Soo <soo@Sooui-MacBookAir.local>
2021-04-07 03:17:03 +0900

    fix: precompile 설정 수정

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.assets.compile = true
+  # config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
